81a2286ad16979ce4b7f4ce1b0b4b43efb688a01
HruthVik <hruthviknaik03@gmail.com>
2024-03-18 11:53:51 +0530

    added the js api part

--- script.js
+++ script.js
@@ -21,7 +21,37 @@ let generateCard=(data)=>{
     console.log(data);
     const hp=data.stats[0].base_stat;
     const imgSrc=data.sprites.other.dream_world.front_default;
-    const pokeName=data.
+    const pokeName=data.name;
+    const statAttack=data.stats[1].base_stat;
+    const statDefence=data.stats[2].base_stat;
+    const statSpeed=data.stats[5].base_stat;
+    card.innerHTML=`
+    <p class="hp">
+          <span>HP</span>
+          ${hp}
+        </p>
+        <div id="img_div">
+          <img src=${imgSrc} alt="" />
+        </div>
+        <h2 class="poke-name">${pokeName}</h2>
+        <div class="types">
+          <span>type 1</span>
+          <span>type 2</span>
+        </div>
+        <div class="stats">
+          <div>
+            <h3>${statAttack}</h3>
+            <p>attacks</p>
+          </div>
+          <div>
+            <h3>${statDefence}</h3>
+            <p>Defense</p>
+          </div>
+          <div>
+            <h3>${statSpeed}</h3>
+            <p>Speed</p>
+          </div>
+        </div>`;
 }
 
 btn.addEventListener("click",getPokedata);
